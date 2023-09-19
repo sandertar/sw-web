95c8b6f1da45ac412d878f3e0da0ce19536830ab
sandertar <sander.taranov@gmail.com>
2023-09-19 13:36:50 +0200

    Added Link to page logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export function Header(): JSX.Element {
   return (
-    <header className="app-layout pt-8 pb-14 flex justify-center">
-      <Image src="/logo.png" alt="Vercel Logo" width={200} height={80} priority />
-    </header>
+    <Link href="/characters">
+      <header className="app-layout pt-8 pb-14 flex justify-center">
+        <Image src="/logo.png" alt="Vercel Logo" width={200} height={80} priority />
+      </header>
+    </Link>
   );
 }
