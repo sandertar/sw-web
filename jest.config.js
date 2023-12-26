9f453fdd561812953241ffe6a67c3b57f5a8d055
Alex Taranov <sander.taranov@gmail.com>
2023-12-26 14:26:56 +0100

    Added github actions

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,8 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['app/**/*.{ts,tsx}', 'utils/**/*.{ts,tsx}'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
